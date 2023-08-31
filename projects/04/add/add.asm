4a43f9dc963e84997cc8d941d1cf4d97a32d35cc
Filip Bengtegård <bengtegardbook@gmail.com>
2023-08-31 08:29:13 +0200

    First commit

---
+++ projects/04/add/add.asm
@@ -0,0 +1,14 @@
+// Program: Add.asm
+// Computes: RAM[2] = RAM[0] + RAM[1]
+// Usage: put values in RAM[0] and in RAM[1]
+	@R0
+	D=M
+
+	@R1
+	D=D+M
+
+	@R2
+	M=D
+(END)
+	@END
+	0;JMP
---
+++ projects/04/sum1ton/2times.asm
@@ -0,0 +1,9 @@
+    @R0
+    D=M
+
+    @R1
+    M=D
+    M=D+M
+(END)
+	@END
+	0;JMP
